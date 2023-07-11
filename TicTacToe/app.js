6326f23446030b033f9ebc386cdd28df65e99cfb
Sabbir Hossain <113119826+sabbir292@users.noreply.github.com>
2023-07-11 08:52:04 +0530

    minimax and alpha beta pruning added

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -8,7 +8,8 @@ let winner = null
 let playersTurn = 'player1'
 let opponent = 'computer'
 let opponentSymbol = 'O'
-let Difficulty = 'easy'
+let Difficulty = 'unbeatable'
+
 
 
 function resetBoard(){
@@ -47,14 +48,19 @@ function players(name,symbol){
 
 const opponents = document.getElementById('opponents')
 const difficulty = document.getElementById('difficulty')
+const turn = document.querySelector('.turn')
+
+
 
 window.addEventListener('DOMContentLoaded',()=>{
   opponents.selectedIndex = 0
-  console.log(opponent)
+  difficulty.selectedIndex = 1
 })
 
 
 opponents.addEventListener('input', selectOpponent)
+difficulty.addEventListener('input', selectDifficulty)
+
 
 function selectOpponent(){
     let selectedPlayer = opponents.value
@@ -64,6 +70,12 @@ function selectOpponent(){
     console.log({opponent,opponentSymbol})
     
     resetBoard()
+}
+
+function selectDifficulty(){
+   Difficulty = difficulty.value
+   console.log(Difficulty)
+   resetBoard()
 }
 
 function possibleMoves(originBoard) {
@@ -94,55 +106,104 @@ function displayGameBoard() {
 }
 displayGameBoard()
 
-
-function bestMove() {
-  let bestScore = -Infinity
-  let bestMove;
-  const freeSpace = possibleMoves(gameboard)
-  // console.log(freeSpace)
-  if (freeSpace.length > 0 && playersTurn === 'computer' && !winner && Difficulty === 'easy') {
-    const randMove = Math.round(Math.random() * (freeSpace.length - 1))
-    const randArr = freeSpace[randMove]
-    moves.push([randArr[0], randArr[1]])
-    gameboard[randArr[0]][randArr[1]] = 'O'
-    updateCellDisplay(randArr[0], randArr[1], "O")
-    playersTurn = 'player1'
-    getWinner(moves)
-  }
-
-  // if(Difficulty === 'unbeatable'){
-  //   console.log('un')
-  //   for(let i = 0; i<freeSpace.length; i++){
-  //     let score = minimax(gameboard,0,false)
-  //     if(score>bestScore){
-  //       bestScore = score
-  //       bestMove = freeSpace[i]
-  //     }
-  //   }
-
-  //   gameboard[bestMove[0]][bestMove[1]] = '0'
-  //   moves.push([bestMove[0],bestMove[1]])
-  //   updateCellDisplay(bestMove[0],bestMove[1],'O')
-  //   playersTurn = 'player1'
-  //   getWinner(moves)
-  // }
-}
-
 const cells = document.querySelectorAll('.cell')
 cells.forEach(cell => {
   cell.addEventListener('click', makeMove)
 })
+function bestMove() {
+  const freeSpace = possibleMoves(gameboard);
 
-// function minimax(board,depth,isMaximising){
-//   // const freeSpace = possibleMoves(gameboard)
- 
-//   let results = getWinner(moves)
-//   if(results){
-//     console.log(results)
-//   }
-  
-//   return 1
-// }
+  if (freeSpace.length > 0 && playersTurn === 'computer' && !winner && Difficulty === 'easy') {
+    const randMove = Math.round(Math.random() * (freeSpace.length - 1));
+    const randArr = freeSpace[randMove];
+    moves.push([randArr[0], randArr[1]]);
+    gameboard[randArr[0]][randArr[1]] = 'O';
+    updateCellDisplay(randArr[0], randArr[1], 'O');
+    playersTurn = 'player1';
+    getWinner(moves);
+  }
+
+  if (Difficulty === 'unbeatable' && freeSpace.length > 0 && !winner) {
+    let bestScore = -Infinity;
+    let bestMove;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (gameboard[i][j] === '') {
+          gameboard[i][j] = 'O';
+          moves.push([i, j]);
+          let score = minimax(gameboard, 0, false, -Infinity, Infinity);
+          moves.pop();
+          gameboard[i][j] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            bestMove = [i, j];
+          }
+        }
+      }
+    }
+
+    gameboard[bestMove[0]][bestMove[1]] = 'O';
+    updateCellDisplay(bestMove[0], bestMove[1], 'O');
+    moves.push([bestMove[0], bestMove[1]]);
+    playersTurn = 'player1';
+    getWinner(moves);
+  }
+}
+
+function minimax(board, depth, isMaximizing, alpha, beta) {
+  const score = getWinner(moves);
+
+  if (winner) {
+    return score;
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = 'O';
+          moves.push([i, j]);
+          let score = minimax(board, depth + 1, false, alpha, beta);
+          moves.pop();
+          board[i][j] = '';
+          bestScore = Math.max(bestScore, score - depth);
+          alpha = Math.max(alpha, score);
+
+          if (beta <= alpha) {
+            break;
+          }
+        }
+      }
+    }
+
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = 'X';
+          moves.push([i, j]);
+          let score = minimax(board, depth + 1, true, alpha, beta);
+          moves.pop();
+          board[i][j] = '';
+          bestScore = Math.min(bestScore, score + depth);
+          beta = Math.min(beta, score);
+
+          if (beta <= alpha) {
+            break;
+          }
+        }
+      }
+    }
+
+    return bestScore;
+  }
+}
 
 
 function makeMove() {
@@ -152,24 +213,30 @@ function makeMove() {
 
   const freeSpace = possibleMoves(gameboard)
 
-  if (freeSpace.length > 0 && playersTurn === 'player1' && !winner &&!this.textContent) {
+  if (freeSpace.length > 0 && playersTurn === 'player1' && !this.textContent && !winner) {
+    console.log('p1')
     const row = parseInt(coArr[0])
     const col = parseInt(coArr[1])
     moves.push([row, col])
     gameboard[row][col] = 'x'
     this.textContent = 'x'
     updateCellDisplay(coArr[0], coArr[1], "X")
+
     if(opponent === 'computer'){
       // console.log(opponent)
       playersTurn = 'computer'
-      bestMove()
+      function delayedFunction() {
+        // Your function logic goes here
+        console.log("Delayed function called after 200ms");
+        bestMove()
+      }
+      
+      setTimeout(delayedFunction, 200);
     }else if(opponent === 'player2'){
       // console.log(opponent)
       playersTurn = 'player2'
     }
-
     getWinner(moves)
-
   }
 
  else if (freeSpace.length > 0 && opponent === 'player2' && playersTurn === 'player2' && !winner) {
@@ -224,26 +291,28 @@ function getWinner(moves) {
 
     if ([rows[row], cols[column], diagonal, antiDiag].includes(3)) {
       winner = 'a'
-      console.log(winner)
       score = -10
-      return {winner,score}
+      console.log({winner,score})
+      return score
     }
     else if([rows[row], cols[column], diagonal, antiDiag].includes(-3)) {
       winner = 'computer'
-      console.log(winner)
       score = 10
-      return {winner,score}
+      console.log({winner,score})
+      return score
     }
     count++
   }
   if (count < 9) {
     console.log('pending')
+    winner = null
     return 'pending'
   }
   else {
     winner = 'draw'
     score = 0
-    return {winner,score}
+    console.log({winner,score})
+    return score
   }
+  
 }
-
