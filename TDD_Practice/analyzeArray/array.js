dc05fbd49b842b5b51997579754d182620bd0366
sabbir292 <113119826+sabbir292@users.noreply.github.com>
2024-02-03 12:46:30 +0530

    added getMin fun

--- TDD_Practice/analyzeArray/array.js
+++ TDD_Practice/analyzeArray/array.js
@@ -7,5 +7,15 @@ const analyzeArray = (array) => {
         return total / array.length
     }
 
+    const getMin = (arr) => {
+        let min = arr[0]
+        arr.forEach(element => {
+          if(element <= min){
+           return min = element
+          }
+        });
+        return min
+    }
+    
 }
 module.exports = analyzeArray
