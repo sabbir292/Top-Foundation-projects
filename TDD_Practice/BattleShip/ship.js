d0d8ec8ca18b7ccb3c0757ea9f0a2bd04cee685a
sabbir292 <113119826+sabbir292@users.noreply.github.com>
2024-02-23 16:23:35 +0530

    added ship factory, hit,isSunk,setposition method

--- TDD_Practice/BattleShip/ship.js
+++ TDD_Practice/BattleShip/ship.js
@@ -1,5 +1,6 @@
 const ship = (length) => {
 let hitCount = 0;
+
     const hit = () => {
         return hitCount += 1;
     }
@@ -8,10 +9,25 @@ let hitCount = 0;
             return true
         }else return false;
     }
+        
+    const setPosition = (initial, axis) =>{
+        const coordinates = []
+        const [a, b] = initial
+        for(let i = 0; i < length; i ++){
+            if(axis.toLowerCase() === 'h'){
+                coordinates.push(a.toString() + i.toString())
+            }else{
+                coordinates.push(i.toString() + b.toString())
+            }
+        }
+        return coordinates
+    }
 
     return{
+        length,
         hit,
         isSunk,
+        setPosition,
     }
 }
 
